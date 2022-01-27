95eb2c2870d8ce43bcf71fc8984b8f469c8c393e
Taylor K Paisie <tpaisie@ufl.edu>
2022-01-27 10:57:15 -0500

    fixed filter fasta script

--- phylophun/manipulate_files/filter_fasta_file.py
+++ phylophun/manipulate_files/filter_fasta_file.py
@@ -19,9 +19,7 @@ from Bio.SeqFeature import SeqFeature
 from ftplib import FTP
 from urllib.error import HTTPError
 
-# input_file = args.input
-# id_file = args.id_list
-# output_file = args.output
+
 
 
 def filter_fasta_file(args):
@@ -29,32 +27,33 @@ def filter_fasta_file(args):
 	print(wanted)
 	print("Found %i unique identifiers in %s" % (len(wanted), args.ids))
 	records = (r for r in SeqIO.parse(args.input, "fasta") if r.id in wanted)
-	count = SeqIO.write(records, args.output, "fasta")
-	print("Saved %i records from %s to %s" % (count, args.input, args.output))
+	count = SeqIO.write(records, "filter_"+args.input, "fasta")
+	print("Saved %i records from %s to %s" % (count, args.input, "filter_"+args.input))
 	if count < len(wanted):
 	    print("Warning %i IDs not found in %s" % (len(wanted) - count, args.input))
 
 	record_ids = list()
 
-	with open(output_file, 'w') as f:
-		for record in SeqIO.parse("filter_"+input_file, 'fasta'):
+	# this part is needed with replicates are in fasta file 
+	with open(args.output, 'w') as f:
+		for record in SeqIO.parse("filter_"+args.input, 'fasta'):
 	 		if record.id not in record_ids:
 	 			record_ids.append(record.id)
 	 			SeqIO.write(record, f, 'fasta')
-	# print("Saved %i records from %s to %s" % (record, input_file, output_file))
+		# print("Saved %i records from %s to %s" % (len(record), args.input, args.output))
 
 
-
+# replicates are first removed and placed in "filter_" file, the taxa in text file will only 
+# be in the output file
 
 def main():
 	parser=argparse.ArgumentParser(description="Filter fasta alignment. Text file is for what sequences to keep in alignment.")
-    #parser=argparse.ArgumentParser(description="Change the names in your fasta file. Make it your way or find your way on the fasta-file highway.")
 	parser.add_argument("-i", help="Input fasta file that needs to be filtered.", 
 		dest='input', type=str, required=True)
 	parser.add_argument("-t",help="Sample IDs of fasta seqs to keep in alignment.", 
 		dest="ids", type=str, required=True)
-	parser.add_argument("-o",help="Output Fasta file.", dest="output", 
-		type=str, required=True)
+	parser.add_argument("-o",help="Filtered output fasta file.", 
+		dest="output", type=str, required=True)
 	parser.set_defaults(func=filter_fasta_file)
 	args=parser.parse_args()
 	args.func(args)
